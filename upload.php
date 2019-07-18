55e1da8a135a595a2509e0c19d84ad4ca55b0ccd
Dai Nguyen <ngocdai94@gmail.com>
2019-07-18 15:35:03 -0700

    update UI and input files

--- upload.php
+++ upload.php
@@ -121,75 +121,77 @@
     </style>
   </head>
   <body>
-    <!-- <form action="upload.php" method="POST" enctype="multipart/form-data">
-        Select file to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input type="submit" value="Upload File" name="submit">
-    </form> -->
+      <header style="text-align:center;">
+        <a href="/">HOME</a>
+      </header>
+      <!-- <form action="upload.php" method="POST" enctype="multipart/form-data">
+          Select file to upload:
+          <input type="file" name="fileToUpload" id="fileToUpload">
+          <input type="submit" value="Upload File" name="submit">
+      </form> -->
 
-    <div id="floating-panel">
-      <?php 
-        for ($i = 1; $i < $size; $i++) {
-          echo "<div><span>Entity ". $i .": </span>\n";
-          echo '<input class="latlng" type="text" value=' . $col_10[$i] . ',' . $col_11[$i] . ">\n";
-          echo '<input class="submit" type="button" value="Reverse Geocode"> </div>' . "\n";
+      <div id="floating-panel">
+        <?php 
+          for ($i = 1; $i < $size; $i++) {
+            echo "<div><span>Entity ". $i .": </span>\n";
+            echo '<input class="latlng" type="text" value=' . $col_10[$i] . ',' . $col_11[$i] . ">\n";
+            echo '<input class="submit" type="button" value="Reverse Geocode"> </div>' . "\n";
+          }
+        ?>
+        <!-- <span>Entity 1: </span>
+        <input id="latlng" type="text" value=<?php //echo $col_10[2] . ',' . $col_11[2];?>>
+        <input id="submit" type="button" value="Reverse Geocode"> -->
+      </div>
+
+      <div id="map"></div>
+      <script>
+        function initMap() {
+          let map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 8,
+            center: {lat: 40.731, lng: -73.997}
+          });
+          let geocoder = new google.maps.Geocoder;
+          let infowindow = new google.maps.InfoWindow;
+
+          // document.getElementByID('submit').addEventListener('click', function() {
+          //   geocodeLatLng(geocoder, map, infowindow);
+          // });
+          let classSubmit = document.getElementsByClassName('submit');
+          let classLatLng = document.getElementsByClassName('latlng');
+          for (let i = 0; i < classSubmit.length; i++) {
+            classSubmit[i].addEventListener('click', function() {
+              geocodeLatLng(geocoder, map, infowindow, classLatLng[i].value);
+            });
+          }
         }
-      ?>
-      <!-- <span>Entity 1: </span>
-      <input id="latlng" type="text" value=<?php //echo $col_10[2] . ',' . $col_11[2];?>>
-      <input id="submit" type="button" value="Reverse Geocode"> -->
-    </div>
 
-    <div id="map"></div>
-    <script>
-      function initMap() {
-        let map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 8,
-          center: {lat: 40.731, lng: -73.997}
-        });
-        let geocoder = new google.maps.Geocoder;
-        let infowindow = new google.maps.InfoWindow;
-
-        // document.getElementByID('submit').addEventListener('click', function() {
-        //   geocodeLatLng(geocoder, map, infowindow);
-        // });
-        let classSubmit = document.getElementsByClassName('submit');
-        let classLatLng = document.getElementsByClassName('latlng');
-        for (let i = 0; i < classSubmit.length; i++) {
-          classSubmit[i].addEventListener('click', function() {
-            geocodeLatLng(geocoder, map, infowindow, classLatLng[i].value);
+        function geocodeLatLng(geocoder, map, infowindow, input) {
+          // let input = document.getElementById('latlng').value;
+          let latlngStr = input.split(',', 2);
+          let latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
+          geocoder.geocode({'location': latlng}, function(results, status) {
+            if (status === 'OK') {
+              console.log(results);
+              if (results[0]) {
+                map.setZoom(16);
+                map.setCenter(latlng);
+                let marker = new google.maps.Marker({
+                  position: latlng,
+                  map: map
+                });
+                infowindow.setContent(results[0].formatted_address);
+                infowindow.open(map, marker);
+              } else {
+                window.alert('No results found');
+              }
+            } else {
+              window.alert('Geocoder failed due to: ' + status);
+            }
           });
         }
-      }
-
-      function geocodeLatLng(geocoder, map, infowindow, input) {
-        // let input = document.getElementById('latlng').value;
-        let latlngStr = input.split(',', 2);
-        let latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
-        geocoder.geocode({'location': latlng}, function(results, status) {
-          if (status === 'OK') {
-            console.log(results);
-            if (results[0]) {
-              map.setZoom(15);
-              map.setCenter(latlng);
-              let marker = new google.maps.Marker({
-                position: latlng,
-                map: map
-              });
-              infowindow.setContent(results[0].formatted_address);
-              infowindow.open(map, marker);
-            } else {
-              window.alert('No results found');
-            }
-          } else {
-            window.alert('Geocoder failed due to: ' + status);
-          }
-        });
-      }
-    </script>
-    <script async defer
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCdsim6obIAp8R4-uVq6H_U1GcZEUr6CxE&libraries=places&callback=initMap">
-    </script>
-
+      </script>
+      <script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCdsim6obIAp8R4-uVq6H_U1GcZEUr6CxE&libraries=places&callback=initMap">
+      </script>
   </body>
 </html>
